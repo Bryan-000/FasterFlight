52ad2bf56f136abd911e27abdd8055d0a1d9c5b9
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-03-08 10:31:34 +0000

    meow

---
+++ src/Patches.cs
@@ -0,0 +1,59 @@
+﻿namespace FasterFlight;
+
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using ULTRAKILL.Cheats;
+using PluginConfig.API.Fields;
+using System.Reflection.Emit;
+
+/// <summary> class with all the patches :33333 </summary>
+[HarmonyPatch]
+public static class Patches
+{
+    /// <summary> Transpile <see cref="Flight.Update"/> so it uses <seealso cref="noclipSpeed"/> and <seealso cref="noclipWeeeeSpeed"/> instead of fixed values of 1f and 2.5f. </summary>
+    [HarmonyTranspiler] [HarmonyPatch(typeof(Flight), "Update")]
+    public static IEnumerable<CodeInstruction> FlightTranspiler(IEnumerable<CodeInstruction> instructions) =>
+        FlightTranspiler(instructions, "flightSpeed", "flightWeeeeSpeed");
+
+    /// <summary> Transpile <see cref="Noclip.UpdateTick"/> so it uses <seealso cref="noclipSpeed"/> and <seealso cref="noclipWeeeeSpeed"/> instead of fixed values of 1f and 2.5f. </summary>
+    [HarmonyTranspiler] [HarmonyPatch(typeof(Noclip), "UpdateTick")]
+    public static IEnumerable<CodeInstruction> NoclipTranspiler(IEnumerable<CodeInstruction> instructions) =>
+        FlightTranspiler(instructions, "noclipSpeed", "noclipWeeeeSpeed");
+
+    /// <summary> Transpiler to be used on <see cref="Flight.Update"/> and <seealso cref="Noclip.UpdateTick"/> </summary>
+    public static IEnumerable<CodeInstruction> FlightTranspiler(IEnumerable<CodeInstruction> instructions, string regularSpeedFieldName, string boostSpeedFieldName)
+    {
+        MethodInfo get_instance = AccessTools.PropertyGetter(typeof(Plugin), "Instance");
+        MethodInfo get_value = AccessTools.PropertyGetter(typeof(FloatField), "value");
+
+        int floatCount = 0;
+        foreach (CodeInstruction instruction in instructions)
+        {
+            if (instruction.opcode == OpCodes.Ldc_R4)
+            {
+                floatCount++;
+
+                // if this is past the second ldc.r4 then just skip everything
+                if (floatCount > 2)
+                    goto yield_meow;
+
+                // put the plugin instance on the evaluation stack aka Plugin.Instance
+                yield return new(OpCodes.Callvirt, get_instance);
+
+                // load instance field from plugin class with the provided names based on if this is the first or second ldc.r4
+                yield return new(OpCodes.Ldfld, AccessTools.Field(typeof(Plugin), floatCount == 1 ? regularSpeedFieldName : boostSpeedFieldName));
+
+                // call property getter for the field to put its (should be) float value onto the evaluation stack to be consumed by whatever was doing ldc.r4
+                // (should be float multiSpeed setter)
+                yield return new(OpCodes.Callvirt, get_value);
+
+                // skip so we dont return the original ldc.r4
+                continue;
+            }
+
+        yield_meow:
+            yield return instruction;
+        }
+    }
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -4,11 +4,7 @@ using BepInEx;
 using HarmonyLib;
 using PluginConfig.API;
 using PluginConfig.API.Fields;
-using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
-using System.Reflection.Emit;
-using ULTRAKILL.Cheats;
 
 /// <summary> General plugin handler. </summary>
 [BepInDependency("com.eternalUnion.pluginConfigurator")]
@@ -22,46 +18,31 @@ public class Plugin : BaseUnityPlugin
     public PluginConfigurator config;
 
     /// <summary> Plugin config float fields for changing the speed of what you fly at. </summary>
-    public FloatField flightSpeed, flightWeeeeSpeed;
+    public FloatField flightSpeed, flightWeeeeSpeed,
+        noclipSpeed, noclipWeeeeSpeed;
 
     /// <summary> Load the mod. </summary>
     public void Awake()
     {
-        config = PluginConfigurator.Create(PluginInfo.Name, PluginInfo.GUID);
-        config.SetIconWithURL(Path.Combine(Path.GetDirectoryName(Info.Location), "icon.png"));
+        Instance = this;
+        LoadConfig();
 
-        flightSpeed = new(config.rootPanel, "Regular Flight speed", "meowmeowmeow mrrrp miaaow rawwrr :3", 1f);
-        flightWeeeeSpeed = new(config.rootPanel, "Boost Flight speed", "meow - tequilla", 2.5f);
-        
-        new Harmony(PluginInfo.GUID).PatchAll(GetType());
+        new Harmony(PluginInfo.GUID).PatchAll(GetType().Assembly);
     }
 
-    /// <summary> Patch Noclip.UpdateTick/Flight.Update so it uses the mods flight speed and boost speed instead. </summary>
-    [HarmonyTranspiler] [HarmonyPatch(typeof(Noclip), "UpdateTick")] [HarmonyPatch(typeof(Flight), "Update")]
-    public static IEnumerable<CodeInstruction> SpeedUpNoclip(IEnumerable<CodeInstruction> instructions)
+    /// <summary> Loads the configurator. </summary>
+    public void LoadConfig()
     {
-        MethodInfo get_instance = AccessTools.PropertyGetter(typeof(Plugin), "Instance");
-        MethodInfo get_value = AccessTools.PropertyGetter(typeof(FloatField), "value");
+        config = PluginConfigurator.Create(PluginInfo.Name, PluginInfo.GUID);
+        config.SetIconWithURL(Path.Combine(Path.GetDirectoryName(Info.Location), "icon.png"));
+        config.presetButtonHidden = true; // fuck u
 
-        int floatCount = 0;
-        foreach (CodeInstruction instruction in instructions)
-        {
-            if (instruction.opcode == OpCodes.Ldc_R4)
-            {
-                floatCount++;
+        ConfigDivision flightDivision = new(config.rootPanel, "Flight");
+        flightSpeed = new(flightDivision, "Regular Flight speed", "meowmeowmeow mrrrp miaaow rawwrr :3", 1f);
+        flightWeeeeSpeed = new(flightDivision, "Boost Flight speed", "meow - tequilla", 2.5f);
 
-                if (floatCount > 2)
-                    goto yield_meow;
-
-                yield return new(OpCodes.Callvirt, get_instance); // put the plugin instance on the evaluation stack aka Plugin.Instance
-                yield return new(OpCodes.Ldfld, AccessTools.Field(typeof(Plugin), floatCount == 1 ? "flightSpeed" : "flightWeeeeSpeed")); // load instance field aka Plugin.Instance.flightSpeed/flightWeeeeSpeed
-                yield return new(OpCodes.Callvirt, get_value); // call property getter for Plugin.Instance.flightSpeed/flightWeeeeSpeed to put its float value onto the evaluation stack to be consumed by `float speedMulti = ;`
-
-                continue;
-            }
-
-        yield_meow:
-            yield return instruction;
-        }
+        ConfigDivision noclipDivision = new(config.rootPanel, "Noclip");
+        noclipSpeed = new(noclipDivision, "Regular Noclip speed", "imhonestlytoolazytoputanythingfunnyhere", 1f);
+        noclipWeeeeSpeed = new(noclipDivision, "Boost Noclip speed", "rawr2.2"/*geometry dash*/, 2.5f);
     }
 }
