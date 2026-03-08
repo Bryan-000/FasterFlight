13d49b25bad3d340f273ff0a8cd342836c47ea60
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-03-08 09:40:49 +0000

    make stuff instance based + comments

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -14,11 +14,14 @@ using ULTRAKILL.Cheats;
 [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 public class Plugin : BaseUnityPlugin
 {
+    /// <summary> Static plugin instance property. </summary>
+    public static Plugin Instance { get; internal set; }
+
     /// <summary> PluginConfigurator instance for this mod :3 </summary>
-    public static PluginConfigurator config;
+    public PluginConfigurator config;
 
     /// <summary> Plugin config float fields for changing the speed of what you fly at. </summary>
-    public static FloatField flightSpeed, flightWeeeeSpeed;
+    public FloatField flightSpeed, flightWeeeeSpeed;
 
     /// <summary> Load the mod. </summary>
     public void Awake()
@@ -35,6 +38,7 @@ public class Plugin : BaseUnityPlugin
     [HarmonyTranspiler] [HarmonyPatch(typeof(Noclip), "UpdateTick")] [HarmonyPatch(typeof(Flight), "Update")]
     public static IEnumerable<CodeInstruction> SpeedUpNoclip(IEnumerable<CodeInstruction> instructions)
     {
+        MethodInfo get_instance = AccessTools.PropertyGetter(typeof(Plugin), "Instance");
         MethodInfo get_value = AccessTools.PropertyGetter(typeof(FloatField), "value");
 
         int floatCount = 0;
@@ -47,8 +51,9 @@ public class Plugin : BaseUnityPlugin
                 if (floatCount > 2)
                     goto yield_meow;
 
-                yield return new(OpCodes.Ldsfld, AccessTools.Field(typeof(Plugin), floatCount == 1 ? "flightSpeed" : "flightWeeeeSpeed"));
-                yield return new(OpCodes.Callvirt, get_value);
+                yield return new(OpCodes.Callvirt, get_instance); // put the plugin instance on the evaluation stack aka Plugin.Instance
+                yield return new(OpCodes.Ldfld, AccessTools.Field(typeof(Plugin), floatCount == 1 ? "flightSpeed" : "flightWeeeeSpeed")); // load instance field aka Plugin.Instance.flightSpeed/flightWeeeeSpeed
+                yield return new(OpCodes.Callvirt, get_value); // call property getter for Plugin.Instance.flightSpeed/flightWeeeeSpeed to put its float value onto the evaluation stack to be consumed by `float speedMulti = ;`
 
                 continue;
             }
